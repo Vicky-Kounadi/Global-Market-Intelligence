3da0f7782bd69b3fb09a27202bb34d57dcf1f4bc
Vasiliki Kounadi <116192912+Vicky-Kounadi@users.noreply.github.com>
2026-05-16 10:41:08 +0300

    Count data

--- EDAQueries.sql
+++ EDAQueries.sql
@@ -4,3 +4,8 @@ USE world;
 SELECT * FROM country LIMIT 10;
 SELECT * FROM city LIMIT 10;
 SELECT * FROM countrylanguage LIMIT 10;
+
+-- DATA COUNT
+SELECT Count(*) AS country_count FROM country;
+SELECT Count(*) AS city_count FROM city;
+SELECT Count(*) AS lang_count FROM countrylanguage;
